b58e8277ef1915fef00e00bf8766aa65355925af
gostekk <gostekk94@gmail.com>
2017-01-28 12:59:49 +0100

    Modified ebin/touch.c | Added changing mtime of inserted files

--- ebin/touch.c
+++ ebin/touch.c
@@ -1,37 +1,37 @@
 /*! \file touch.c
  *  \author Grzegorz Jaworski
  *  \date 28 Styczeń 2017
- *  \version   v0.1
- *  \brief Tworzenie nowego pliku
- *  \details Program wyświetlajacy zawartość wskazanych plików
+ *  \version   v0.2
+ *  \brief Zmiana czasu modyfikacji pliku
+ *  \details Program zmieniający czas ostatniej modyfikacji podanych plików.
  *  @see https://github.com/HiImTrixie/shell-jj
  *
  *
  *
  */
-#include <stdio.h>
+#include <sys/stat.h>
+#include <time.h>
+#include <utime.h>
+
+
 
 int main(int argc, char **argv)
 {
 	FILE *file;
 	int c,j;
-	if(argv[1] == NULL)
-	{
-		fprintf(stderr, "No arguments were given.\n");
-		return 1;
-	}
-	else if(argv[1] && argv[2] == NULL)
-	{
-		file = fopen(argv[1], "w");
-		fclose(file);
-	}
-	else
-	{
-		for(int i = 1; argv[i] != NULL; i++)
-		{
-			file = fopen(argv[i], "w");
-			fclose(file);
-		}
-	}
-	return 1;
+	file = fopen(argv[1], "w");
+	fclose(file);
+
+  struct stat foo;
+  time_t mtime;
+  struct utimbuf new_times;
+
+  stat(argv[1], &foo);
+  mtime = foo.st_mtime;
+
+  new_times.actime = foo.st_atime;
+  new_times.modtime = time(NULL);
+  utime(argv[1], &new_times);
+
+  return 0;
 }
