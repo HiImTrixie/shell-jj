3087f6e5d9ae94e0624ab6dcf100e9b962516902
gostekk <gostekk94@gmail.com>
2017-01-28 18:31:13 +0100

    Fix ebin/man.c | Wiped code and write it again

--- ebin/man.c
+++ ebin/man.c
@@ -1,7 +1,7 @@
 /*! \file man.c
  *  \author Grzegorz Jaworski
  *  \date 28 Styczeń 2017
- *  \version   v0.1
+ *  \version   v0.2
  *  \brief Wyświetl dokumentacje funkcji
  *  \details Program wyświetlający dokumentacje wskazanego pliku jako argument.
  *  @see https://github.com/HiImTrixie/shell-jj
@@ -10,76 +10,36 @@
  *
  */
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+void read_(char *name)
+{
+  FILE *file;
+	int c;
+  char dest[64]="../man/";
+  strcat(dest,name);
+  strcat(dest,".txt");
+  file = fopen(dest, "r");
+  if(file)
+  {
+    while((c = getc(file)) != EOF)
+      putchar(c);
+    fclose(file);
+  }else{
+    printf("No manual entry for %s\n", name );
+  }
+}
 
 int main(int argc, char **argv)
 {
-	FILE *fp;
-	int c;
 	if(argv[1] == NULL)
 	{
 		printf("You have to type what function you want to check.\n");
 		printf("Example: man ls\n");
+    exit(0);
 	}
-	else if(strcmp(argv[1], "cd") == 0)
-	{
-		fp = fopen("../man/cd.txt", "r");
-		if(fp)
-		{
-			while((c = getc(fp)) != EOF)
-				putchar(c);
-			fclose(fp);
-		}
-	}
-	else if(strcmp(argv[1], "exit") == 0)
-	{
-		fp = fopen("../man/exit.txt", "r");
-		if(fp)
-		{
-			while((c = getc(fp)) != EOF)
-				putchar(c);
-			fclose(fp);
-		}
-	}
-	else if(strcmp(argv[1], "pwd") == 0)
-	{
-		fp = fopen("../man/pwd.txt", "r");
-		if(fp)
-		{
-			while((c = getc(fp)) != EOF)
-				putchar(c);
-			fclose(fp);
-		}
-	}
-	else if(strcmp(argv[1], "ls") == 0)
-	{
-		fp = fopen("../man/ls.txt", "r");
-		if(fp)
-		{
-			while((c = getc(fp)) != EOF)
-				putchar(c);
-			fclose(fp);
-		}
-	}
-	else if(strcmp(argv[1], "man") == 0)
-	{
-		fp = fopen("../man/man.txt", "r");
-		if(fp)
-		{
-			while((c = getc(fp)) != EOF)
-				putchar(c);
-			fclose(fp);
-		}
-	}
-	else if(strcmp(argv[1], "cat") == 0)
-	{
-		fp = fopen("../man/cat.txt", "r");
-		if(fp)
-		{
-			while((c = getc(fp)) != EOF)
-				putchar(c);
-			fclose(fp);
-		}
-	}
+  read_(argv[1]);
   putchar('\n');
 	return 1;
 }
